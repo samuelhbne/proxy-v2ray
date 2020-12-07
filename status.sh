5c9e1b61d55285f7a88a4cc6d840e0413e158977
Samuel Huang <samuelh2006@gmail.com>
2020-12-07 14:44:58 +1100

    V2ray config file relocation

--- status.sh
+++ status.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-ADDRESS=`cat /tmp/client.json|jq -r ' ."outbounds"[0]."settings"."vnext"[0]."address" '`
-PORT=`cat /tmp/client.json|jq -r ' ."outbounds"[0]."settings"."vnext"[0]."port" '`
-UUID=`cat /tmp/client.json|jq -r ' ."outbounds"[0]."settings"."vnext"[0]."users"[0]."id" '`
-ALTERID=`cat /tmp/client.json|jq -r ' ."outbounds"[0]."settings"."vnext"[0]."users"[0]."alterId" '`
+ADDRESS=`cat /etc/v2ray/client.json|jq -r ' ."outbounds"[0]."settings"."vnext"[0]."address" '`
+PORT=`cat /etc/v2ray/client.json|jq -r ' ."outbounds"[0]."settings"."vnext"[0]."port" '`
+UUID=`cat /etc/v2ray/client.json|jq -r ' ."outbounds"[0]."settings"."vnext"[0]."users"[0]."id" '`
+ALTERID=`cat /etc/v2ray/client.json|jq -r ' ."outbounds"[0]."settings"."vnext"[0]."users"[0]."alterId" '`
 
 V2RAYINFO=`echo "{'add':'$ADDRESS','aid':'$ALTERID','id':'$UUID','net':'tcp','port':'$PORT','ps':'VLP-V2RAY'}"`
 V2RAYINFO=`echo $V2RAYINFO|base64|tr -d '\n'`
